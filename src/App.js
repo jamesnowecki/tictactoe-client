a5bfeaeea026a41ba3e1ab56ac40eb66fb3413f2
James Nowecki <jamesnowecki@gmail.com>
2021-03-06 16:53:38 +0000

    can fetch a clientId from the server with websocket and set as state!!!!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
 import styles from './App.module.scss';
+import React, { useState, useEffect } from 'react';
+import {w3cwebsocket as W3CWebSocket } from 'websocket';
 
-function App() {
+const client = new W3CWebSocket('ws://127.0.0.1:1984')
+
+
+
+const App = () => {
+
+  const [clientId, updateClientId] = useState('');
+  const [gameId, updateGameId] = useState('');
+  const [boardState, updateBoardState] = useState({});
+
+  const checkClientId = (id) => {
+    console.log("checkId called with", id)
+    if (id !== clientId) {
+      updateClientId(id);
+    }
+  }
+
+  useEffect(() => {
+
+    const establishSocketConnection = () => {
+      client.onopen = () => {
+        console.log('websocket open');
+      }
+      client.onmessage = (message) => {
+        console.log(message);
+
+        const data = JSON.parse(message.data)
+        checkClientId(data.clientId)
+      }
+    };
+
+    establishSocketConnection();
+    
+
+  })
+
   return (
     <div className={styles.App}>
       <p>Tic tac toe</p>
