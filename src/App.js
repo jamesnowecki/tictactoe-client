7b7e8c927d439ba5b8a782a75710f58e186fa4e0
James Nowecki <jamesnowecki@gmail.com>
2021-03-06 17:13:48 +0000

    created switch case for testing methods

--- src/App.js
+++ src/App.js
@@ -4,20 +4,43 @@ import {w3cwebsocket as W3CWebSocket } from 'websocket';
 
 const client = new W3CWebSocket('ws://127.0.0.1:1984')
 
-
-
 const App = () => {
 
   const [clientId, updateClientId] = useState('');
   const [gameId, updateGameId] = useState('');
   const [boardState, updateBoardState] = useState({});
 
-  const checkClientId = (id) => {
-    console.log("checkId called with", id)
-    if (id !== clientId) {
-      updateClientId(id);
+  const executeMethod = (dataObj) => {
+    const { method } = dataObj.method;
+
+    switch (method) {
+      case 'create':
+        console.log('create method recieved')
+        console.log('data:'. dataObj)
+        break;
+      case 'join':
+        console.log('join method received')
+        console.log('data:'. dataObj)
+        break;
+      case 'update':
+        console.log('update method received')
+        console.log('data:, dataObj')
     }
   }
+
+  const checkClientId = (id) => {
+    console.log("checkId called with", id);
+    if (id !== clientId) {
+      updateClientId(id);
+    };
+  };
+
+  const checkGameId = (id) => {
+    console.log("checkGameId called with", id);
+    if (id !== gameId) {
+      updateGameId(id);
+    };
+  };
 
   useEffect(() => {
 
@@ -25,11 +48,18 @@ const App = () => {
       client.onopen = () => {
         console.log('websocket open');
       }
+
+      client.onclose = () => {
+        console.log('websocket closed')
+      }
+
       client.onmessage = (message) => {
         console.log(message);
 
         const data = JSON.parse(message.data)
-        checkClientId(data.clientId)
+        
+        executeMethod(data)
+        
       }
     };
 
